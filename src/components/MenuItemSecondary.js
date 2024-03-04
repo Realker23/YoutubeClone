c5885ba59b8daf32d4ec2170a416ffc117f13a39
Abhishek Roushan <abhishekashu23@gmail.com>
2024-03-04 23:45:12 +0530

    V2.1

--- src/components/MenuItemSecondary.js
+++ src/components/MenuItemSecondary.js
@@ -4,10 +4,11 @@ import {Link} from "react-router-dom";
 
 const MenuItemSecondary = () => {
   return (
-    <div className="text-white">
-      <GoHomeFill />
-      <span className="px-4 py-2">
-        <Link to={"/"}>Home</Link>{" "}
+    <div className="text-white mx-4 text-lg">
+      <span className="px-4 py-2 text-center text-xl">
+        <Link to={"/"}>
+          <GoHomeFill />
+        </Link>
       </span>
     </div>
   );
