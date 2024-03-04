8c7baa92ea2a28e5cf0b2c85c376e00266f83e3e
Abhishek Roushan <abhishekashu23@gmail.com>
2024-03-04 23:40:38 +0530

    V2.1

--- src/components/MenuItemSecondary.js
+++ src/components/MenuItemSecondary.js
@@ -1,7 +1,16 @@
 import React from "react";
+import {GoHomeFill} from "react-icons/go";
+import {Link} from "react-router-dom";
 
 const MenuItemSecondary = () => {
-  return <div className="text-white">SidebarSecondary</div>;
+  return (
+    <div className="text-white">
+      <GoHomeFill />
+      <span className="px-4 py-2">
+        <Link to={"/"}>Home</Link>{" "}
+      </span>
+    </div>
+  );
 };
 
 export default MenuItemSecondary;
